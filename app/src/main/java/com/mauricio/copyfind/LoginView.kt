97e53b37d89d535f6d59e57fb744d012b4250e2a
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-03-21 20:35:09 -0600

    Se agrega caja del texto del email y boton

--- app/src/main/java/com/mauricio/copyfind/LoginView.kt
+++ app/src/main/java/com/mauricio/copyfind/LoginView.kt
@@ -1,9 +1,47 @@
 package com.mauricio.copyfind
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun LoginView() {
     Text(text = "Login")
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = username,
+            placeholder = {
+                Text("email")
+            },
+            onValueChange = { username = it },
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            shape = RoundedCornerShape(10.dp)
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Enviar")
+        }
+    }
+
 }
